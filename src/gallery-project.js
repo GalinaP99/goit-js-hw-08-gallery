58f93b96266bbb49e176dc6a579710c534b41633
Galina Pankevych <gln99@ukr.net>
2021-01-25 16:15:17 +0200

    додає закриття модального вікна

--- src/gallery-project.js
+++ src/gallery-project.js
@@ -7,7 +7,7 @@ import images from "./gallery-items.js";
                 data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg" alt="Tulips" />
         </a>
     </li>*/
-   // const galleryRef = document.querySelector('js-gallery');
+   
 const createImagesList = images => {
     const titleRef = document.createElement('li');
     titleRef.classList.add('gallery__item');
@@ -32,29 +32,53 @@ const imagesList = images.map(image =>
 const refs = {
     gallery: document.querySelector('.js-gallery'),
     largeImage: document.querySelector('.lightbox__image'),
-    openModal: document.querySelector('.js-lightbox')
+    openModal: document.querySelector('.js-lightbox'),
+    closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
+    closeModalOverlay: document.querySelector('.lightbox__overlay')
 }
-    
+ 
 refs.gallery.append(...imagesList);
+
 refs.gallery.addEventListener('click', onGalleryClick);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.closeModalOverlay.addEventListener('click', onOverlayClose);
+window.addEventListener('keyup', clickESC);
+ 
 function onGalleryClick(event) {
     event.preventDefault();
     const imageRef = event.target;
-    console.log("event target: ", imageRef);
+   // console.log("event target: ", imageRef);
     if (imageRef.nodeName !== "IMG") {
         return;
-    }
-    const largeImageURL = imageRef.dataset.source;
-     console.log("edataset source: ",largeImageURL);
+    } 
+   const largeImageURL = imageRef.dataset.source;
+    const largeImageALT = imageRef.alt;
+     console.log("dataset source: ",largeImageURL);
     refs.largeImage.src = largeImageURL;
-    console.log(largeImageURL);
+    refs.largeImage.alt = largeImageALT;
+    refs.openModal.classList.add('is-open');
     //setLargeImageSrc(largeImageURL);
 }
-
 /*function setLargeImageSrc(url) {
     refs.largeImage.src = url;
+}*/
+function onCloseModal() {
+    refs.openModal.classList.remove('is-open');
+     refs.largeImage.src = "";
+    refs.largeImage.alt = "";
 }
-refs.openModal.addEventListener("click", toggleModal)
+
+function onOverlayClose(event) {
+    if (event.target === event.currentTarget) {
+        onCloseModal();
+    }
+}
+function clickESC(event) {
+    if (event.code === "Escape") {
+        onCloseModal();
+    }
+}
+/*refs.openModal.addEventListener("click", toggleModal)
   refs.closeModalBtn.addEventListener("click", toggleModal)
 
   function toggleModal() {
